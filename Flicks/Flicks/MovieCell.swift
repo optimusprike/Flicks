53da0a440ef89066c6467c43a76c562b7e1c653e
Prakash Pudhucode <optimusprike@users.noreply.github.com>
2017-02-05 23:09:06 -0500

    Add files via upload
    final project

---
+++ Flicks/Flicks/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Prakash Pudhucode on 1/30/17.
+//  Copyright © 2017 Prakash Pudhucode. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var movieImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flicks/Flicks/MoviesViewController.swift
@@ -0,0 +1,88 @@
+//
+//  MoviesViewController.swift
+//  Flicks
+//
+//  Created by Prakash Pudhucode on 1/30/17.
+//  Copyright © 2017 Prakash Pudhucode. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+import MBProgressHUD
+
+class MoviesViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    var movies: [NSDictionary]?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let refreshControl = UIRefreshControl()
+        // Do any additional setup after loading the view.
+        
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+        loadData(refreshControl: refreshControl)
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        loadData(refreshControl: refreshControl)
+    }
+    
+    func loadData(refreshControl: UIRefreshControl){
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let data = data {
+                if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    print(responseDictionary)
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    self.movies = (responseDictionary["results"] as! [NSDictionary])
+                    
+                    self.tableView.reloadData()
+                    refreshControl.endRefreshing()
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movies = movies {
+            return movies.count
+        }else{
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        let movie = movies![indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        let baseURL = "https://image.tmdb.org/t/p/w500/"
+        let posterpath  = movie["poster_path"] as! String
+        
+        let imageURL = NSURL(string: baseURL + posterpath)
+        
+        
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        cell.movieImageView.setImageWith(imageURL as! URL)
+        
+        
+        print("row \(indexPath.row)")
+        return cell
+    }
+    
+}
